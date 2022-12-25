787a41259267e21f7da1748439a1dec996c43623
Anh Nguyễn <nnquyenanh1@gmail.com>
2022-12-25 19:39:51 +0700

    aa

---
+++ lib/home/option.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class option extends StatelessWidget {
+  const option({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/shop.webp"),
+                fit: BoxFit.cover,
+                opacity: 0.5),
+          ),
+          child: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("OPTION",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      letterSpacing: 2)),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Center(
+                        child: TextButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (_) => option()));
+                      },
+                      child: Text("ADMIN", style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize:20
+                )),
+                    ))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Center(
+                       child: TextButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (_) => option()));
+                      },
+                      child: Text("USER", style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize:20
+                )),
+                    )
+                    )),
+              ),
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:shopaoquan/Login/dangky.dart';
 import 'package:shopaoquan/Login/dangnhap.dart';
 import 'package:shopaoquan/home/home.dart';
+import 'package:shopaoquan/home/option.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: dangnhap()
+    home: option()
   ));
 }
